8f43b17697f10bef22ebf3986bb3af8d9fcaed2d
Deneene Whitaker <lisaelizrdo@gmail.com>
2023-03-23 20:01:22 -0500

    dooo

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const express = require('express');
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/notes', (req, res) =>
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
   res.send(
     `index.html`
   )
