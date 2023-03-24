bac593bbfa5bf863574be0227866fb683868fb4e
Deneene Whitaker <lisaelizrdo@gmail.com>
2023-03-23 19:54:51 -0500

    push

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const PORT = process.env.PORT || 3015;
+
 app.get('/notes', (req, res) =>
   res.send(
     `index.html`
   )
 );
 
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
